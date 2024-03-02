85c4dbe0f97ebde40dad5c5aa3bd435cc8e114a4
KARTIK PANDEY <kartikpandey07@KARTIKs-MacBook-Air.local>
2024-03-02 13:56:24 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,13 @@
-import Navbar from "./components/Navbar";
-import NavbarL from "./components/NavbarL";
-import Login from "./pages/login";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-  Navigate,
-} from "react-router-dom";
-import Register from "./pages/register";
-import Home from "./pages/Home";
+
+
 function App() {
-  const currentUser=false;
-  const Layout=()=>{
-    return (
-      //<QueryClientProvider client={queryClient}>
-       <div className="bg-[#86B6F6]">
-       {!currentUser?
-      <Navbar/>:<NavbarL/>}
-        <Outlet/>
-        </div>
-       // </QueryClientProvider>
-    );
-
-  };
-
-  const router = createBrowserRouter([
-    {
-      path:"/",
-      element:
-      <Layout/>,
-      children:[
-        {
-          path:"/",
-          element:<Home/>
-        },
-      ]
-    },
-    {
-      path: "/login",
-      element: <Login/>,
-    },
-    {
-      path: "/register",
-      element: <Register/>,
-    },
-    
-   
-  ]);
  
-    return <div >
-       <RouterProvider router={router} />
-    </div>;
-  } 
+
+  return (
+   <div>
+    
+   </div>
+  )
+}
 
 export default App
