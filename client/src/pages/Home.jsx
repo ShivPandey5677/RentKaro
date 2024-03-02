badfd36b187311db59eb2214845bf9a9a30d9944
userabhishek19 <121710381+userabhishek19@users.noreply.github.com>
2024-03-02 20:50:52 +0530

    enhanced UserCard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import PropertyCard from '../components/PropertyCard'
+
 import Properties from '../components/Properties'
-import UserCard from './UserCard'
+
 
 
 const Home = () => {
