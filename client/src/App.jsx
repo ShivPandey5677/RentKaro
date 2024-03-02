e9596cb83e09ea2c940d6f5be4de6d32e9f83c95
ShivPandey5677 <pandeyshiv525@gmail.com>
2024-03-02 12:16:37 +0530

    RRouting added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,48 @@
-
+import Navbar from "./components/Navbar";
+import Login from "./pages/login";
 
 function App() {
- 
+  const currentUser=true;
+  const Layout=()=>{
+    return (
+      //<QueryClientProvider client={queryClient}>
+       <div className="bg-[#86B6F6]">
+       {!currentUser?
+      <Navbar/>:<NavBarL/>}
+        <Outlet/>
+        </div>
+       // </QueryClientProvider>
+    );
 
-  return (
-   <div>
+  };
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:
+      <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
     
-   </div>
-  )
-}
+   
+  ]);
+ 
+    return <div >
+       <RouterProvider router={router} />
+    </div>;
+  } 
 
 export default App
---
+++ client/src/components/NavbarL.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarL = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavbarL
