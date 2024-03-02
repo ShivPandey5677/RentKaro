7beb94258c8f420d8bc5040ec9b3603501df8178
userabhishek19 <121710381+userabhishek19@users.noreply.github.com>
2024-03-02 16:48:37 +0530

    Added Property View

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import PropertyCard from '../components/PropertyCard'
+
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       
+
+     
     </div>
   )
 }
