1cdb0c959c8b5e5c1968fccf5778014ee7bfac8f
ShivPandey5677 <pandeyshiv525@gmail.com>
2024-03-02 12:52:39 +0530

    Navbar Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import Navbar from "./components/Navbar";
+import NavbarL from "./components/NavbarL";
 import Login from "./pages/login";
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Register from "./pages/register";
+import Home from "./pages/Home";
 function App() {
-  const currentUser=true;
+  const currentUser=false;
   const Layout=()=>{
     return (
       //<QueryClientProvider client={queryClient}>
        <div className="bg-[#86B6F6]">
        {!currentUser?
-      <Navbar/>:<NavBarL/>}
+      <Navbar/>:<NavbarL/>}
         <Outlet/>
         </div>
        // </QueryClientProvider>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
 
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineInfoCircle, AiOutlineLogin, AiOutlineUser, AiOutlineUserAdd, AiOutlineUserSwitch, AiOutlineMail } from 'react-icons/ai';
+
+// 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+    
+        
+      <nav className="flex flex-row items-center justify-between bg-slate-900 text-white py-4 px-6">
+        <div  className="logo-container">
+        <h1>LOGO</h1>
+      </div>
+        <div className="flex items-center">
+          <span className="text-3xl font-bold"></span>
+        </div>
+        <div className="hidden lg:flex md:flex lg:flex-1 items-center justify-end font-normal space-x-6">
+          <Link to="/" spy={true} smooth={true} className="hover:bg-slate-800 hover:rounded py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineHome size={20} /> Home
+          </Link>
+          <Link to="/About" spy={true} smooth={true} className="hover:bg-slate-800 hover:rounded py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineInfoCircle size={20}/> About
+          </Link>
 
-export default Navbar
+          <Link to="/psychiatrist" spy={true} smooth={true} className="hover:bg-slate-800 hover:rounded py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineUserSwitch size={20}/> Psychiatrist
+          </Link>
+          <Link to="/register" spy={true} smooth={true} className="hover:bg-teal-800 hover:text-white-500 hover:rounded  transition py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineUserAdd size={20}/> <b>SignUp</b>
+          </Link>
+
+          <Link to="/login" spy={true} smooth={true} className="hover:bg-teal-800  hover:text-white-500 hover:rounded transition py-2 px-4 hover:scale-110 ease-in duration-200">
+            <AiOutlineLogin size={20}/><b> LogIn</b>
+          </Link>
+        </div>
+        <div className="lg:hidden block bg-slate-900 text-white py-4 px-6 ">
+          <ul className="flex text-center text-xl space-x-6">
+           
+          </ul>
+        </div>
+      </nav>
+      
+    );
+  };
+  
+  export default Navbar;
